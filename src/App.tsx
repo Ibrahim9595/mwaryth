fb83dfec262f79067feebfe55977e9bf512e8b34
ibrahim mamdouh <i2771995@gmail.com>
2021-02-07 13:43:41 +0200

    data-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import "./App.css";
-import "semantic-ui-css/semantic.min.css";
 import { NewNodeForm } from "./forms/NewNodeForm";
 import { useTranslation } from "react-i18next";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import Tree from "./components/Tree";
 
 function App() {
   const { i18n } = useTranslation();
   return (
     <div className="App">
+      <div className="tree">
+        <Tree />
+      </div>
       <div className="form-container">
         <h3>{i18n.t("EDIT_DATA")}</h3>
         <NewNodeForm
@@ -18,7 +22,6 @@ function App() {
           }}
         />
       </div>
-      <div className="tree">TEST</div>
     </div>
   );
 }
